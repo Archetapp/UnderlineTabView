d18d14e673635d2aac3ecf272e37cbfda5a2694a
Jared Davidson (Archetapp) <archetapp@gmail.com>
2021-10-11 16:25:47 -0600

    Initial Commit

---
+++ Shared/UnderlineTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderlineTabViewApp.swift
+//  Shared
+//
+//  Created by Jared Davidson on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct UnderlineTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
